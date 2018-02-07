39bd88da3ff524bdea9972b53f2176e569344c56
Albert <albert.cheu@gmail.com>
2018-02-07 11:59:42 -0500

    Horizon plot beginning

---
+++ horizon.py
@@ -0,0 +1,39 @@
+def horizonHelper(ax,x,y,numLayers,color):
+    ## find how much vertical space each layer will take
+    ymin,ymax = min(y),max(y)
+    step = (ymax-ymin) / numLayers
+
+    ## each layer is a different plot
+    for i in range(numLayers):
+        yi = []
+        for yval in y:
+            if yval >= ymin+i*step and yval <= ymin+(i+1)*step:
+                yi.append(yval - (ymin+i*step))
+                pass
+            elif yval < ymin+i*step: yi.append(0)
+            else: yi.append(step)
+            pass
+        ax.plot(x,yi,color=color,alpha=(i+1)/numLayers)
+        
+        pass
+    
+    pass
+
+def horizon(ax,x,y,numLayers):
+    ##positives
+    pos = []
+    for yval in y:        
+        if yval >= 0: pos.append(yval)
+        else: pos.append(0)
+        pass
+    horizonHelper(ax,x,pos,numLayers,'blue')
+    
+    ##negatives
+    neg = []
+    for yval in y:
+        if yval < 0: neg.append(-yval)
+        else: neg.append(0)
+        pass
+    horizonHelper(ax,x,neg,numLayers,'red')
+
+    pass
--- hw2-part2-threePlots.py
+++ hw2-part2-threePlots.py
@@ -7,18 +7,19 @@ import numpy as np
 import matplotlib.dates as dates
 import datetime as dt
 
-from splom import splom as splom
+from horizon import horizon
+#from splom import splom
 
 ## loop through the csv's
 filenames = ['DowJones','NASDAQ','NYSE','SP500']
 suffix = '-HistoricalPrices.csv'
 
-scores = []
+values = []
 changes = []
 days = []
 for fname in filenames:
 
-    scores.append([])
+    values.append([])
     changes.append([])
     days.append([])
     
@@ -28,28 +29,59 @@ for fname in filenames:
 
     ##reverse the list to be in chronological order
     lines.reverse()
-    initScore = 0
+    initValue = 0
     for line in lines:
 
         ## record the date
         row = line.split(',')
         days[-1].append(dt.datetime.strptime(row[0],'%m/%d/%y').date())
 
-        ##record score
-        score = float(row[-1])
-        scores[-1].append(score)
+        ##record value
+        value = float(row[-1])
+        values[-1].append(value)
         
         ## record percent changed
-        if initScore == 0:        
-            initScore = score
+        if initValue == 0:        
+            initValue = value
             pass
-        changes[-1].append((score - initScore) / initScore)
+        changes[-1].append((value - initValue) / initValue)
 
         pass
     pass
 
 
 ## Figure 1: Small-multiple line charts
+def fig1(plt,days,values):
+    fig, axes = plt.subplots(2,2)
+
+    ## plot each index
+    for i in range(4):
+        
+        ## display MM/DD/YYYY
+        axes[i/2,i%2].xaxis.set_major_formatter(dates.DateFormatter('%m/%d/%y'))
+    
+        ## label each new day/month/year
+        axes[i/2,i%2].xaxis.set_major_locator(dates.MonthLocator())
+        
+        ## plot the i-th time series
+        axes[i/2,i%2].plot(days[i],values[i])
+        axes[i/2,i%2].grid()
+        ##axes[i/2,i%2].legend()
+        axes[i/2,i%2].set(title=filenames[i])
+        if i%2 == 0: axes[i/2,i%2].set(ylabel='Value')
+        pass
+
+    plt.gcf().autofmt_xdate()
+    # ax.set(xlabel='Time',
+    #       ylabel='Value',
+    #       title='Stock Market Indices from 2017 to 2018')
+    
+    ## display and save
+    plt.show()
+    #plt.savefig('stockMarket-Line.png')
+    pass
+
+#### Figure 2: Horizon graphs
 fig, axes = plt.subplots(2,2)
 
 ## plot each index
@@ -61,24 +93,19 @@ for i in range(4):
     ## label each new day/month/year
     axes[i/2,i%2].xaxis.set_major_locator(dates.MonthLocator())
     
-    ## pair the i-th time series with its name
-    axes[i/2,i%2].plot(days[i],changes[i])
+    ## break up the i-th time series into quartiles
+    #axes[i/2,i%2].plot(days[i],changes[i])
+    horizon(axes[i/2,i%2],days[i],changes[i],4)
+    
     axes[i/2,i%2].grid()
     ##axes[i/2,i%2].legend()
+
     axes[i/2,i%2].set(title=filenames[i])
     if i%2 == 0: axes[i/2,i%2].set(ylabel='Value')
     pass
 
 plt.gcf().autofmt_xdate()
-#ax.set(xlabel='Time',
-#       ylabel='Value',
-#       title='Stock Market Indices from 2017 to 2018')
-
-
-splom()
 
 ## display and save
-#plt.show()
-#plt.savefig('stockMarket-Line.png')
-
-fig = splom()
+plt.show()
+#plt.savefig('stockMarket-Horizon.png')
