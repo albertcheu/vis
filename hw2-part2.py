aa5d9c9a8b4190c97bf32c4901a916a83a44d611
Albert <albert.cheu@gmail.com>
2018-02-09 13:58:02 -0500

    updated first-part code, bar plot

--- hw2-part2.py
+++ hw2-part2.py
@@ -3,13 +3,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Date plotting obtained from https://stackoverflow.com/a/9627970
+#Date plotting modified from https://stackoverflow.com/a/9627970
 import matplotlib.dates as dates
 import datetime as dt
-
-numdays = 30
-
-fig, ax = plt.subplots()
 
 ## loop through the csv's
 filenames = ['DowJones','NASDAQ','NYSE','SP500']
@@ -45,37 +41,60 @@ for fname in filenames:
         pass
     pass
 
-## display MM/DD/YYYY
-plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%m/%d/%Y'))
+def linePlot(plt):
+    fig, ax = plt.subplots()
 
-## label each new day/month/year
-plt.gca().xaxis.set_major_locator(dates.MonthLocator())
+    ## display MM/DD/YYYY
+    plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%m/%d/%Y'))
+    
+    ## label each new day/month/year
+    plt.gca().xaxis.set_major_locator(dates.MonthLocator())
+    
+    ## plot each index    
+    for i in range(4):
+        ## pair the i-th time series with its name
+        ax.plot(days[i],changes[i],label=filenames[i])
+        pass
+    
+    plt.gcf().autofmt_xdate()
 
-## plot each index
-width =0.23
-ind = np.arange(numdays)
-for i in range(4):
-    ## pair the i-th time series with its name
+    #labels, title, legend
+    ax.set(xlabel='Time',ylabel='Fractional change',
+           title='Stock Market Indices from 2017 to 2018')
+    ax.grid()
+    ax.legend()
 
-    ## Swap for line plot
-    ax.plot(days[i],changes[i],label=filenames[i])
-    #ax.bar(ind + i*width, changes[i], width, label=filenames[i])
+    ## display and save
+    plt.show()
+    plt.savefig('stockMarket-Line.png')
+    pass
+
+#linePlot(plt)
+
+def barPlot(plt):
+    ## number of days to display, width of bar, and list from 0 to numdays-1
+    numdays = 30
+    width =0.23
+    ind = np.arange(numdays)
+
+    ## Plot the data (make wide enough to see tick labels)
+    fig, ax = plt.subplots(1,1,figsize=(8,4))
+    for i in range(4):
+        ax.bar(ind + i*width, changes[i][:numdays], width, label=filenames[i])
+        pass
+    
+    ## Labels, title, and legend
+    ax.set_xticks(ind)
+    ax.set(xlabel='Business Days since 2 Feb',
+           ylabel='Fractional change',
+           title='Stock Market Indices from 3 Feb to 17 March, 2017')
+    ax.grid()
+    ax.legend()
+
+    ## display and save
+    plt.show()
+    plt.savefig('stockMarket-Bar.png')
     
     pass
 
-plt.gcf().autofmt_xdate()
-#ax.set_xticks(ind)
-ax.set(#xlabel='Business Days since 2 Feb',
-       xlabel='Time',
-       ylabel='Fractional change',
-       ## Swap for line plot
-       title='Stock Market Indices from 2017 to 2018')
-       #title='Stock Market Indices from 3 Feb to 17 March, 2017')
-
-ax.grid()
-ax.legend()
-
-## display and save
-#plt.show()
-plt.savefig('stockMarket-Line.png')
-#plt.savefig('stockMarket-Bar.png')
+barPlot(plt)
