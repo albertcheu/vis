2cc3d65cacc83d3c613ffb804b9192bc9ef4f4ba
Albert <albert.cheu@gmail.com>
2018-03-22 21:34:50 -0400

    new homework

---
+++ hw3-part3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#scatter plot: assessed value against total living area
+#each point is a black circle
+#fill white for oldest
+#fill black for newest
+#for intermediary values, interpolate luminance between
+
+
+#bar chart: number of buildings with each A/C type
+#Blue-Black-Red color indicates A/C type
+
+
+#find the q-tiles of the assessed values
+#map each q-tile to a color on the red-blue gradient
+#stacked bar plot: bar height is number of buildings with each view quality
+#blue-red color gradient encodes distribution of assessed value
