9ef92b4abff247c156ef5857c4905bd22cecf1b0
Albert <albert.cheu@gmail.com>
2018-03-22 23:18:52 -0400

    Scatter plot mostly done

--- hw3-part3.py
+++ hw3-part3.py
@@ -14,32 +14,49 @@ for i in range(1,len(lines)):
     row = lines[i].strip().split(',')
     
     if row[11-1] in ('R1','R2','R3','R4'):#K
-        try:
-            value.append(int(row[20-1]))#T
-            area.append(int(row[26-1]))#Z
-            year.append(int(row[23-1]))#W
-            view.append(row[50-1])#R_VIEW = AX
-            ac.append(row[44-1])#R_AC = AR
+
+        #outlier
+        if int(row[26-1]) > 10000:
+            continue
+        
+        value.append(int(row[20-1]))#T
+        area.append(int(row[26-1]))#Z
+        year.append(int(row[23-1]))#W
+        view.append(row[50-1])#R_VIEW = AX
+        
+        ac_type = row[44-1]
+        color = 'gray'
+        if ac_type in ('C','D'):
+            color = 'red'
             pass
-        except:
-            print row
-            print len(row)
-            break
+        ac.append(color)#R_AC = AR
+        
         pass
+
     pass
 
+n = len(value)
 
+#categorical
 #scatter plot: assessed value against total living area
-#each point is a black circle
-#fill white for oldest
-#fill black for newest
-#for intermediary values, interpolate luminance between
-def linePlot(plt):
-    
-    #plt.gcf().autofmt_xdate()
+#Blue-Black-Red color indicates A/C type
 
+def valueVsArea(plt):
+    
+    #move the red points to the end (draw on top)
+    back = n-1
+    for i in range(n):
+        if i!=back and ac[i] == 'red':
+            area[i], area[back] = (area[back],area[i])
+            value[i], value[back] = (value[back],value[i])
+            ac[i], ac[back] = (ac[back],ac[i])
+            back -= 1
+            pass
+        pass
+    
     ## labels, title, legend
-    plt.scatter(area, value, c="g")
+    
+    plt.scatter(area, value, c=ac,marker='.')
     plt.xlabel("Total living area (square feet)")
     plt.ylabel("Total property value")
     #plt.legend(loc=2)
@@ -49,12 +66,15 @@ def linePlot(plt):
     #plt.savefig('value-vs-area.png')
     pass
 
+#valueVsArea(plt)
 
-#bar chart: number of buildings with each A/C type
-#Blue-Black-Red color indicates A/C type
+#continuous
+#stacked bar plot: one bar per ac type, height for number of parcels
+#each stack is age range (i.e. decade built)
 
 
-#find the q-tiles of the assessed values
-#map each q-tile to a color on the red-blue gradient
-#stacked bar plot: bar height is number of buildings with each view quality
-#blue-red color gradient encodes distribution of assessed value
+#diverging
+#determine min,median,max values; map to blue,purple,red
+#segment values into five bins
+#stacked bar plot: for each view quality, total bar height is no. of buildings
+
